758e0856de7d97542cd6fef2bfcbaeca9b418081
zhedongzheng <zhedongzheng@outlook.com>
2017-03-25 14:31:03 +0000

    small change

--- tensorflow-models/rnn_regr_test.py
+++ tensorflow-models/rnn_regr_test.py
@@ -7,15 +7,18 @@ TIME_STEPS = 20
 BATCH_SIZE = 50  
 
 
-def get_data_batch():
+def get_data_batch(return_x=False):
     global BATCH_START, TIME_STEPS
     # xs shape (50batch, 20steps)
     xs = np.arange(BATCH_START, BATCH_START+TIME_STEPS*BATCH_SIZE).reshape((BATCH_SIZE, TIME_STEPS)) / (10*np.pi)
     seq = np.sin(xs)
     res = np.cos(xs)
-    BATCH_START += TIME_STEPS
+    BATCH_START += TIME_STEPS*BATCH_SIZE
     # returned seq, res: shape (batch, step, input)
-    return (seq[:, :, np.newaxis], res[:, :, np.newaxis])
+    if return_x:
+        [seq[:, :, np.newaxis], res[:, :, np.newaxis], xs]
+    else:
+        return (seq[:, :, np.newaxis], res[:, :, np.newaxis])
 
 
 if __name__ == '__main__':
@@ -23,4 +26,4 @@ if __name__ == '__main__':
     train_data = data[:1500]
     test_data = data[1500:]
     regr  = RNNRegressor(n_in=1, n_step=TIME_STEPS, n_hidden=16, n_out=1)
-    regr.fit(train_data, batch_size=50, test_data=test_data)
+    regr.fit(train_data, BATCH_SIZE, test_data)

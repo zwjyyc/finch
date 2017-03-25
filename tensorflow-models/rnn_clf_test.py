88d25438804948c59502a7be93b52577e5eea8b6
zhedongzheng <zhedongzheng@outlook.com>
2017-03-25 21:53:08 +0000

    normalization ( / 255.0) helps so much

--- tensorflow-models/rnn_clf_test.py
+++ tensorflow-models/rnn_clf_test.py
@@ -20,13 +20,15 @@ def plot(log, dir='./log'):
 
 if __name__ == '__main__':
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
-    X_train = X_train.reshape(-1, 28, 28)
-    X_test = X_test.reshape(-1, 28, 28)
+    # normalization ( / 255.0) provides great effect here
+    X_train = (X_train / 255.0).reshape(-1, 28, 28)
+    X_test = (X_test / 255.0).reshape(-1, 28, 28)
     y_train = to_one_hot(y_train)
     y_test = to_one_hot(y_test)
 
     clf = RNNClassifier(n_in=28, n_step=28, n_out=10)
-    log = clf.fit(X_train, y_train, (X_test, y_test), n_epoch=30, batch_size=100, en_exp_decay=True)
+    log = clf.fit(X_train, y_train, (X_test, y_test), n_epoch=20, batch_size=100, en_exp_decay=True,
+                  keep_prob_tuple=(1.0, 1.0) )
     pred = clf.predict(X_test)
     clf.close()
     final_acc = np.equal(np.argmax(pred, 1), np.argmax(y_test[:len(pred)], 1)).astype(float).mean()

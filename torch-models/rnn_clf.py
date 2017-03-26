2fe0bf875ad8392870771d74b9ff1436f216e5a3
zhedongzheng <zhedongzheng@outlook.com>
2017-03-27 00:27:28 +0100

    numpy conversion is difficult in torch

--- torch-models/rnn_clf.py
+++ torch-models/rnn_clf.py
@@ -1,42 +1,43 @@
 import torch 
-import torch.nn as nn
+from torch import nn
 from torch.autograd import Variable
+import numpy as np
 
 
 class RNNClassifier(nn.Module):
-    def __init__(self, input_size, sequence_length, hidden_size, num_layers, num_classes):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(RNNClassifier, self).__init__()
         self.input_size = input_size
-        self.sequence_length = sequence_length
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
-    
+        self.num_classes = num_classes
         self.build_model()
+    # end constructor
+
     
     def forward(self, x):
         h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) # set initial states  
         c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)) # set initial states 
-        
         out, _ = self.lstm(x, (h0, c0)) # forward propagate
-        
         out = self.fc(out[:, -1, :]) # decode hidden state of last time step
         return out
+    # end method forward
+
 
     def build_model(self):
+        self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, batch_first=True)
+        self.fc = nn.Linear(self.hidden_size, self.num_classes)
         self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+    # end method build_model
 
-    def fit(self, train_dataset, num_epochs, batch_size):
-        train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                                   batch_size=batch_size, 
-                                                   shuffle=True)
 
+    def fit(self, X, y, num_epochs, batch_size):
         for epoch in range(num_epochs):
-            for i, (images, labels) in enumerate(train_loader):
-                images = Variable(images.view(-1, self.sequence_length, self.input_size))
-                labels = Variable(labels)
+            i = 0
+            for X_train_batch, y_train_batch in zip(self.gen_batch(X, batch_size), self.gen_batch(y, batch_size)):
+                images = Variable(torch.from_numpy(X_train_batch.astype(np.float32)))
+                labels = Variable(torch.from_numpy(y_train_batch.astype(np.int64)))
                 
                 # forward + backward + optimize
                 self.optimizer.zero_grad()
@@ -44,8 +45,35 @@ class RNNClassifier(nn.Module):
                 loss = self.criterion(outputs, labels)
                 loss.backward()
                 self.optimizer.step()
-                
+                i+=1
                 if (i+1) % 100 == 0:
-                    print ('Epoch [%d/%d], Step [%d/%d], Loss: %.4f' 
-                           %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
+                    print ('Epoch [%d/%d], Step [%d/%d], Loss: %.5f'
+                           %(epoch+1, num_epochs, i+1, int(len(X)/batch_size), loss.data[0]))
+    # end method fit
+
+
+    def evaluate(self, X_test, y_test, batch_size):
+        correct = 0
+        total = 0
+        for X_test_batch, y_test_batch in zip(self.gen_batch(X_test, batch_size),
+                                              self.gen_batch(y_test, batch_size)):
+            images = Variable(torch.from_numpy(X_test_batch.astype(np.float32)))
+            labels = torch.from_numpy(y_test_batch.astype(np.int64))
+            outputs = self.forward(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum()
+        print('Test Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total)) 
+    # end method evaluate
+
+
+    def gen_batch(self, arr, batch_size):
+        if len(arr) % batch_size != 0:
+            new_len = len(arr) - len(arr) % batch_size
+            for i in range(0, new_len, batch_size):
+                yield arr[i : i + batch_size]
+        else:
+            for i in range(0, len(arr), batch_size):
+                yield arr[i : i + batch_size]
+    # end method gen_batch
 
--- torch-models/rnn_clf_test.py
+++ torch-models/rnn_clf_test.py
@@ -1,33 +1,17 @@
-import torchvision.datasets as dsets
-import torchvision.transforms as transforms
 from rnn_clf import RNNClassifier
-
+from keras.datasets import mnist
 
 # Hyper Parameters
-sequence_length = 28
 input_size = 28
 hidden_size = 128
 num_layers = 2
 num_classes = 10
 batch_size = 100
 num_epochs = 2
-learning_rate = 0.01
 
 # MNIST Dataset
-train_dataset = dsets.MNIST(root='../data/',
-                            train=True, 
-                            transform=transforms.ToTensor(),
-                            download=True)
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+rnn = RNNClassifier(input_size, hidden_size, num_layers, num_classes)
+rnn.fit(X_train, y_train, num_epochs, batch_size)
+rnn.evaluate(X_test, y_test, batch_size)
 
-test_dataset = dsets.MNIST(root='../data/',
-                           train=False, 
-                           transform=transforms.ToTensor())
-"""
-# Data Loader (Input Pipeline)
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=batch_size, 
-                                          shuffle=False)
-"""
-rnn = RNNClassifier(input_size, sequence_length, hidden_size, num_layers, num_classes)
-rnn.fit(train_dataset, num_epochs, batch_size)
-    

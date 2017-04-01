3d8e9fdcdef70b3d7f98ba266efff78d8d2cb4ff
zhedongzheng <zhedongzheng@outlook.com>
2017-04-01 16:24:03 +0100

    update

--- tensorflow-models/rnn_shakespeare_test.py
+++ tensorflow-models/rnn_shakespeare_test.py
@@ -26,40 +26,31 @@ def load_text():
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
     print('Loading Shakespeare Data')
-    # Check if file is downloaded.
-    if not os.path.isfile(os.path.join(data_dir, data_file)):
+    if not os.path.isfile(os.path.join(data_dir, data_file)): # check if file is downloaded
         print('Not found, downloading Shakespeare texts from www.gutenberg.org')
         shakespeare_url = 'http://www.gutenberg.org/cache/epub/100/pg100.txt'
-        # Get Shakespeare text
-        response = requests.get(shakespeare_url)
+        response = requests.get(shakespeare_url) # get Shakespeare text
         shakespeare_file = response.content
-        # Decode binary into string
-        s_text = shakespeare_file.decode('utf-8')
-        # Drop first few descriptive paragraphs.
-        s_text = s_text[7675:]
-        # Remove newlines
-        s_text = s_text.replace('\r\n', '')
-        s_text = s_text.replace('\n', '')
-        # Write to file
-        with open(os.path.join(data_dir, data_file), 'w') as out_conn:
+        s_text = shakespeare_file.decode('utf-8') # decode binary into string
+        s_text = s_text[7675:] # drop first few descriptive paragraphs
+        s_text = s_text.replace('\r\n', '') # remove newlines
+        s_text = s_text.replace('\n', '') # remove newlines
+        with open(os.path.join(data_dir, data_file), 'w') as out_conn: # write to file
             out_conn.write(s_text)
     else:
-        # If file has been saved, load from that file
-        with open(os.path.join(data_dir, data_file), 'r') as file_conn:
+        with open(os.path.join(data_dir, data_file), 'r') as file_conn: # If file has been saved, load from that file
             s_text = file_conn.read().replace('\n', '')
     return s_text
 
 
-def build_vocab(characters):
-    character_counts = collections.Counter(characters)
-    # Create vocab --> index mapping
-    chars = character_counts.keys()
-    vocab_to_ix_dict = {key:(ix+1) for ix, key in enumerate(chars)}
-    # Add unknown key --> 0 index
-    vocab_to_ix_dict['_unknown']=0
-    # Create index --> vocab mapping
-    ix_to_vocab_dict = {val:key for key,val in vocab_to_ix_dict.items()}
-    return(ix_to_vocab_dict, vocab_to_ix_dict)
+def build_vocab(word_list, min_word_freq=5):
+    word_counts = collections.Counter(word_list)
+    word_counts = {key:val for key,val in word_counts.items() if val>min_word_freq}
+    words = word_counts.keys()
+    word2idx = {key:(idx+1) for idx,key in enumerate(words)} # create word --> index mapping
+    word2idx['_unknown'] = 0 # add unknown key --> 0 index
+    idx2word = {val:key for key,val in word2idx.items()} # create index --> word mapping
+    return(idx2word, word2idx)
 
 
 def clean_text(s_text):
@@ -68,15 +59,15 @@ def clean_text(s_text):
     return s_text
 
 
-def convert_text_to_word_vecs(char_list, vocab2idx):
-    s_text_ix = []
-    for x in char_list:
+def convert_text_to_word_vecs(word_list, word2idx):
+    s_text_idx = []
+    for word in word_list:
         try:
-            s_text_ix.append(vocab2idx[x])
+            s_text_idx.append(word2idx[word])
         except:
-            s_text_ix.append(0)
-    s_text_ix = np.array(s_text_ix)
-    return s_text_ix
+            s_text_idx.append(0)
+    s_text_idx = np.array(s_text_idx)
+    return s_text_idx
 
 
 def create_batch(s_text_ix):
@@ -99,15 +90,15 @@ if __name__ == '__main__':
     word_list = s_text.split()
 
     print('Building Shakespeare Vocab by Characters')
-    idx2vocab, vocab2idx = build_vocab(word_list)
-    vocab_size = len(idx2vocab)
+    idx2word, word2idx = build_vocab(word_list)
+    vocab_size = len(idx2word)
     print('Vocabulary Length = {}'.format(vocab_size))
-    assert(len(idx2vocab) == len(vocab2idx)) # sanity Check
+    assert(len(idx2word) == len(word2idx)) # sanity Check
 
-    s_text_idx = convert_text_to_word_vecs(word_list, vocab2idx)
+    s_text_idx = convert_text_to_word_vecs(word_list, word2idx)
     
     batch_list = create_batch(s_text_idx)
     X = batch_list
-    y = [np.roll(batch, -1, axis=0) for batch in batch_list]
+    y = [np.roll(batch, -1, axis=1) for batch in batch_list]
     model = RNNLangModel(n_hidden=128, n_layers=2, vocab_size=vocab_size, seq_len=training_seq_len)
     model.fit(X, y)

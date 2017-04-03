40e2d313de462e71dcf16e3497f811ff7a421e28
zhedongzheng <zhedongzheng@outlook.com>
2017-04-03 15:39:50 +0100

    minor change

--- tensorflow-models/conv_clf_test.py
+++ tensorflow-models/conv_clf_test.py
@@ -26,10 +26,10 @@ if __name__ == '__main__':
     y_test = to_one_hot(y_test)
 
     clf = ConvClassifier(28, 28, n_out=10)
-    log = clf.fit(X_train, y_train, batch_size=100, n_epoch=2, keep_prob=0.5, val_data=(X_test, y_test))
+    log = clf.fit(X_train, y_train, n_epoch=1, keep_prob=0.5, val_data=(X_test, y_test))
     pred = clf.predict(X_test)
     clf.close()
     final_acc = np.equal(np.argmax(pred, 1), np.argmax(y_test, 1)).astype(float).mean()
     print("final testing accuracy: %.4f" % final_acc)
 
-    plot(log)
+    # plot(log)

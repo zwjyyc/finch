f5cf94a0db447b280ee56a29d0644029d3beda18
zhedongzheng <zhedongzheng@outlook.com>
2017-04-14 21:37:15 +0100

    minor change

--- tensorflow-models/rnn_clf_test.py
+++ tensorflow-models/rnn_clf_test.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
     y_test = to_one_hot(y_test)
 
     sess = tf.Session()
-    clf = RNNClassifier(n_in=28, n_step=28, n_hidden=128, n_out=10, n_layer=2, sess=sess, stateful=True)
-    log = clf.fit(X_train, y_train, n_epoch=2, en_exp_decay=True, keep_prob_tuple=(0.5,1.0),
+    clf = RNNClassifier(n_in=28, n_step=28, n_hidden=128, n_out=10, n_layer=2, sess=sess, stateful=False)
+    log = clf.fit(X_train, y_train, n_epoch=10, en_exp_decay=True, keep_prob_tuple=(0.5,1.0),
                   val_data=(X_test,y_test))
     pred = clf.predict(X_test)
     tf.reset_default_graph()
@@ -36,4 +36,4 @@ if __name__ == '__main__':
     final_acc = np.equal(np.argmax(pred,1), np.argmax(y_test,1)).astype(float).mean()
     print("final testing accuracy: %.4f" % final_acc)
 
-    #plot(log)
+    plot(log)

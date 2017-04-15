195f3ad5e3105c173dd993d1e27c3c13c669b47d
zhedongzheng <zhedongzheng@outlook.com>
2017-04-15 18:40:09 +0100

    initial comment

---
+++ tensorflow-models/mlp_clf_cifar10_test.py
@@ -0,0 +1,38 @@
+from utils import to_one_hot
+from keras.datasets import cifar10
+from mlp_clf import MLPClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import os, sys
+import tensorflow as tf
+
+
+def plot(log, dir='./log'):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    sns.set(style='white')
+    plt.plot(log['loss'], label='train_loss')
+    plt.plot(log['val_loss'], label='test_loss')
+    plt.legend(loc='best')
+    plt.savefig(os.path.join(dir, sys.argv[0][:-3]))
+    print("Figure created !")
+
+
+if __name__ == '__main__':
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+    X_train = (X_train/255.0).mean(axis=3).reshape(-1, 32*32)
+    X_test = (X_test/255.0).mean(axis=3).reshape(-1, 32*32)
+    y_train = to_one_hot(y_train)
+    y_test = to_one_hot(y_test)
+
+    sess = tf.Session()
+    clf = MLPClassifier(n_in=32*32, hidden_unit_list=[512, 256, 128], n_out=10, sess=sess)
+    log = clf.fit(X_train, y_train, n_epoch=20, en_exp_decay=True, val_data=(X_test,y_test), dropout=1.0)
+    pred = clf.predict(X_test)
+    tf.reset_default_graph()
+    final_acc = np.equal(np.argmax(pred,1), np.argmax(y_test,1)).astype(float).mean()
+    print("final testing accuracy: %.4f" % final_acc)
+
+    plot(log)

f8b34d47e982b183c2a70c9ef8ce5a99e729d15c
zhedongzheng <zhedongzheng@outlook.com>
2017-04-15 01:41:05 +0100

    minor change

--- tensorflow-models/utils.py
+++ tensorflow-models/utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
 
 
 def to_one_hot(y, num_classes=None):
@@ -10,8 +9,3 @@ def to_one_hot(y, num_classes=None):
     one_hot = np.zeros((n, num_classes))
     one_hot[np.arange(n), y] = 1
     return one_hot
-
-
-def load_mnist():
-    mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
-    return mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels

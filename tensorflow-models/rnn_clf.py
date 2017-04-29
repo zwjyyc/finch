50bcda135cf609592bff7ccf797615c44c21834e
zhedongzheng <zhedongzheng@outlook.com>
2017-04-29 13:39:26 +0100

    fix batch_size feed_dict issue

--- tensorflow-models/rnn_clf.py
+++ tensorflow-models/rnn_clf.py
@@ -117,16 +117,16 @@ class RNNClassifier:
             for X_train_batch, y_train_batch in zip(self.gen_batch(X_train, batch_size),
                                                     self.gen_batch(y_train, batch_size)):
                 lr = self.decrease_lr(en_exp_decay, global_step, n_epoch, len(X), batch_size)
-                if self.stateful:
+                if (self.stateful) and (len(X_train_batch) == batch_size):
                     _, next_state, loss, acc = self.sess.run([self.train_op, self.final_state, self.loss, self.acc],
                         feed_dict = {self.X:X_train_batch, self.y:y_train_batch,
                                      self.in_keep_prob:keep_prob_tuple[0], self.out_keep_prob:keep_prob_tuple[1],
-                                     self.batch_size:batch_size, self.lr:lr, self.init_state:next_state})
+                                     self.batch_size:len(X_train_batch), self.lr:lr, self.init_state:next_state})
                 else:             
                     _, loss, acc = self.sess.run([self.train_op, self.loss, self.acc],
                         feed_dict = {self.X:X_train_batch, self.y:y_train_batch,
                                      self.in_keep_prob:keep_prob_tuple[0], self.out_keep_prob:keep_prob_tuple[1],
-                                     self.batch_size:batch_size, self.lr:lr})
+                                     self.batch_size:len(X_train_batch), self.lr:lr})
                 local_step += 1
                 global_step += 1
                 if local_step % 50 == 0:
@@ -138,14 +138,15 @@ class RNNClassifier:
                 next_state = self.sess.run(self.init_state, feed_dict={self.batch_size:batch_size})
                 for X_test_batch, y_test_batch in zip(self.gen_batch(val_data[0], batch_size),
                                                       self.gen_batch(val_data[1], batch_size)):
-                    if self.stateful:
+                    if (self.stateful) and (len(X_test_batch) == batch_size):
                         v_loss, v_acc, next_state = self.sess.run([self.loss, self.acc, self.final_state],
                             feed_dict = {self.X:X_test_batch, self.y:y_test_batch,
-                                         self.batch_size:batch_size, self.init_state:next_state,
+                                         self.batch_size:len(X_test_batch), self.init_state:next_state,
                                          self.in_keep_prob:1.0, self.out_keep_prob:1.0})
                     else:
                         v_loss, v_acc = self.sess.run([self.loss, self.acc], 
-                            feed_dict = {self.X:X_test_batch, self.y:y_test_batch, self.batch_size:batch_size,
+                            feed_dict = {self.X:X_test_batch, self.y:y_test_batch,
+                                         self.batch_size:len(X_test_batch),
                                          self.in_keep_prob:1.0, self.out_keep_prob:1.0})
                     val_loss_list.append(v_loss)
                     val_acc_list.append(v_acc)
@@ -173,31 +174,24 @@ class RNNClassifier:
     def predict(self, X_test, batch_size=128):
         batch_pred_list = []
         next_state = self.sess.run(self.init_state, feed_dict={self.batch_size:batch_size})
-        for X_test_batch in self.gen_batch(X_test, batch_size, is_size_equal=False):
+        for X_test_batch in self.gen_batch(X_test, batch_size):
             if (self.stateful) and (len(X_test_batch) == batch_size):
                 batch_pred, next_state = self.sess.run([self.logits, self.final_state], 
-                    feed_dict = {self.X:X_test_batch, self.batch_size:batch_size, self.in_keep_prob:1.0,
-                        self.out_keep_prob:1.0, self.init_state:next_state})
+                                         feed_dict = {self.X:X_test_batch, self.batch_size:len(X_test_batch),
+                                                      self.in_keep_prob:1.0, self.out_keep_prob:1.0,
+                                                      self.init_state:next_state})
             else:
-                batch_pred = self.sess.run(self.logits, feed_dict = {self.X:X_test_batch,
-                    self.batch_size:len(X_test_batch), self.in_keep_prob:1.0, self.out_keep_prob:1.0})
+                batch_pred = self.sess.run(self.logits,
+                                           feed_dict = {self.X:X_test_batch, self.batch_size:len(X_test_batch),
+                                                        self.in_keep_prob:1.0, self.out_keep_prob:1.0})
             batch_pred_list.append(batch_pred)
         return np.concatenate(batch_pred_list)
     # end method predict
 
 
-    def gen_batch(self, arr, batch_size, is_size_equal=True):
-        if is_size_equal:
-            if len(arr) % batch_size != 0:
-                new_len = len(arr) - len(arr) % batch_size
-                for i in range(0, new_len, batch_size):
-                    yield arr[i : i+batch_size]
-            else:
-                for i in range(0, len(arr), batch_size):
-                    yield arr[i : i+batch_size]
-        else:
-            for i in range(0, len(arr), batch_size):
-                yield arr[i : i+batch_size]
+    def gen_batch(self, arr, batch_size):
+        for i in range(0, len(arr), batch_size):
+            yield arr[i : i+batch_size]
     # end method gen_batch
 
 

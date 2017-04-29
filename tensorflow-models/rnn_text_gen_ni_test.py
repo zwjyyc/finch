39242e59f3ff564ba88ed9bd87667c0b56d59a93
zhedongzheng <zhedongzheng@outlook.com>
2017-04-29 19:09:56 +0100

    add resolution arg

--- tensorflow-models/rnn_text_gen_ni_test.py
+++ tensorflow-models/rnn_text_gen_ni_test.py
@@ -5,24 +5,25 @@ from utils import clean_text, build_vocab, convert_text_to_idx
 from rnn_text_gen import RNNTextGen
 
 
-BATCH_SIZE = 128
-SEQ_LEN = 40
+BATCH_SIZE = 32
+SEQ_LEN = 20
 NUM_LAYER = 3
 CELL_SIZE = 128
 prime_texts = [
-    'the falseness of an opinion is not for us any objection to it',
-    'that which causes philosophers to be regarded half-distrustfully',
-    'it has gradually become clear to me what every great philosophy up till now'
+    'an opinion is',
+    'that which causes',
+    'it has gradually become clear'
 ]
 
 
 if __name__ == '__main__':
     path = get_file('nietzsche.txt', origin='https://s3.amazonaws.com/text-datasets/nietzsche.txt')
-    text = open(path).read().replace('\n', '')
+    text = open(path).read().replace('\n', ' ')
 
     print('Cleaning Text')
     text = clean_text(text)
-    all_word_list = list(text) # now we are doing char-level, use .split() for word-level
+        all_word_list = list(text)
+        all_word_list = text.split()
 
     print('Building Nietzsche Vocab by Characters')
     idx2word, word2idx = build_vocab(all_word_list)
@@ -37,13 +38,11 @@ if __name__ == '__main__':
     sess = tf.Session()
     with tf.variable_scope('training'):
         train_model = RNNTextGen(cell_size=CELL_SIZE, n_layers=NUM_LAYER,
-                                 vocab_size=vocab_size, seq_len=SEQ_LEN,
                                  sess=sess)
     with tf.variable_scope('training', reuse=True):
         sample_model = RNNTextGen(cell_size=CELL_SIZE, n_layers=NUM_LAYER,
-                                  vocab_size=vocab_size, seq_len=1,
                                   sess=sess)
     log = train_model.fit(X, n_epoch=25, batch_size=BATCH_SIZE,
                           en_exp_decay=True,
-                          sample_pack=(sample_model, idx2word, word2idx, 100, prime_texts))
+                          sample_pack=(sample_model, idx2word, word2idx, 10, prime_texts))
     
--- tensorflow-models/rnn_text_gen_sh_test.py
+++ tensorflow-models/rnn_text_gen_sh_test.py
@@ -1,18 +1,20 @@
 import os, sys, requests
 import numpy as np
-import random
-import matplotlib.pyplot as plt
-import seaborn as sns
 import tensorflow as tf
 from utils import clean_text, build_vocab, convert_text_to_idx
 from rnn_text_gen import RNNTextGen
 
 
-BATCH_SIZE = 100
+BATCH_SIZE = 128
 SEQ_LEN = 50
 NUM_LAYER = 3
 CELL_SIZE = 128
-prime_texts = ['thou art more', 'to be or not to', 'wherefore art thou']
+RESOL = 'word'
+prime_texts = [
+    'thou art more',
+    'to be or not to',
+    'wherefore art thou'
+]
 
 
 def load_text():
@@ -32,13 +34,13 @@ def load_text():
         shakespeare_file = response.content
         s_text = shakespeare_file.decode('utf-8') # decode binary into string
         s_text = s_text[7675:] # drop first few descriptive paragraphs
-        s_text = s_text.replace('\r\n', '') # remove newlines
-        s_text = s_text.replace('\n', '') # remove newlines
+        s_text = s_text.replace('\r\n', ' ') # remove newlines
+        s_text = s_text.replace('\n', ' ') # remove newlines
         with open(os.path.join(data_dir, data_file), 'w') as out_conn: # write to file
             out_conn.write(s_text)
     else:
         with open(os.path.join(data_dir, data_file), 'r') as file_conn: # If file has been saved, load from that file
-            s_text = file_conn.read().replace('\n', '')
+            s_text = file_conn.read().replace('\n', ' ')
     return s_text
 # end function load_text()
 
@@ -47,7 +49,10 @@ if __name__ == '__main__':
     text = load_text()
     print('Cleaning Text')
     text = clean_text(text)
-    all_word_list = list(text) # now we are doing char-level, use .split() for word-level
+    if RESOL == 'char':
+        all_word_list = list(text)
+    if RESOL == 'word':
+        all_word_list = text.split()
 
     print('Building Shakespeare Vocab by Characters')
     idx2word, word2idx = build_vocab(all_word_list)
@@ -57,17 +62,18 @@ if __name__ == '__main__':
 
     all_word_idx = convert_text_to_idx(all_word_list, word2idx)
     X = np.resize(all_word_idx, [int(len(all_word_idx)/SEQ_LEN), SEQ_LEN])
+    print('X shape: ', X.shape)
     
     sess = tf.Session()
     with tf.variable_scope('training'):
         train_model = RNNTextGen(cell_size=CELL_SIZE, n_layers=NUM_LAYER,
-                                 vocab_size=vocab_size, seq_len=SEQ_LEN,
+                                 vocab_size=vocab_size, seq_len=SEQ_LEN, resolution=RESOL,
                                  sess=sess)
     with tf.variable_scope('training', reuse=True):
-        sample_model = RNNTextGen(cell_size=CELL_SIZE, n_layers=NUM_LAYER,
+        sample_model = RNNTextGen(cell_size=CELL_SIZE, n_layers=NUM_LAYER, resolution=RESOL,
                                   vocab_size=vocab_size, seq_len=1,
                                   sess=sess)
     log = train_model.fit(X, n_epoch=25, batch_size=BATCH_SIZE,
                           en_exp_decay=True,
-                          sample_pack=(sample_model, idx2word, word2idx, 30, prime_texts))
+                          sample_pack=(sample_model, idx2word, word2idx, 10, prime_texts))
     

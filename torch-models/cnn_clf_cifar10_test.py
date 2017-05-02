3c67985686291c68b297f346e16acc5df7ec966d
zhedongzheng <zhedongzheng@outlook.com>
2017-05-02 16:10:05 +0100

    add new test

---
+++ torch-models/cnn_clf_cifar10_test.py
@@ -0,0 +1,23 @@
+from cnn_clf import CNNClassifier
+from keras.datasets import cifar10
+
+
+img_size = (32,32)
+img_ch = 3
+kernel_size = 5
+pool_size = 2
+n_out = 10
+batch_size = 128
+n_epoch = 2
+
+
+if __name__ == '__main__':
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    X_train = (X_train / 255.0).reshape(-1, img_ch, img_size[0], img_size[1])
+    X_test = (X_test / 255.0).reshape(-1, img_ch, img_size[0], img_size[1])
+    y_train = y_train.ravel()
+    y_test = y_test.ravel()
+
+    cnn = CNNClassifier(img_size=img_size, img_ch=img_ch, kernel_size=kernel_size, pool_size=pool_size, n_out=n_out)
+    cnn.fit(X_train, y_train, n_epoch, batch_size)
+    cnn.evaluate(X_test, y_test, batch_size)

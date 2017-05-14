3a5a33aaa418cdca3f0309013e943f7ee152fd37
zhedongzheng <zhedongzheng@outlook.com>
2017-05-14 17:23:08 +0100

    by default not filter out any word

--- tensorflow-models/rnn/rnn_text_gen.py
+++ tensorflow-models/rnn/rnn_text_gen.py
@@ -8,7 +8,7 @@ import collections
 
 
 class RNNTextGen:
-    def __init__(self, sess, text, seq_len=50, min_freq=500, cell_size=128, n_layer=3, stateful=True,
+    def __init__(self, sess, text, seq_len=50, min_freq=None, cell_size=128, n_layer=3, stateful=True,
                  stopwords=None):
         """
         Parameters:

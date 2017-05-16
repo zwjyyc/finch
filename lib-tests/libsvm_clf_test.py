3ee52035a866ac66a934920990613bd656d6f871
zhedongzheng <zhedongzheng@outlook.com>
2017-05-16 22:59:32 +0100

    make a wrapper for libsvm

--- lib-tests/libsvm_clf_test.py
+++ lib-tests/libsvm_clf_test.py
@@ -1,20 +1,16 @@
-from svmutil import *
+from libsvm_clf import SVC
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 import numpy as np
 
 
-X, y = make_classification(5000)
-X = X.tolist()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+if __name__ == '__main__':
+    X, y = make_classification(5000)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-problem = svm_problem(y_train, X_train)
-param = svm_parameter()
-param.kernel_type = RBF
-param.C = 1
+    svc = SVC()
+    svc.fit(X_train, y_train)
+    y_pred = svc.predict(X_test)
 
-model = svm_train(problem, param)
-y_pred, _, _ = svm_predict([0]*len(X_test), X_test, model)
-
-acc = np.equal(y_pred, y_test).astype(float).mean()
-print('The testing accuracy is %.3f' % acc)
+    acc = np.equal(y_pred, y_test).astype(float).mean()
+    print('The testing accuracy is %.3f' % acc)

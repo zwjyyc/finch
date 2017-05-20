72636fbbf36f74dcca24d404ba43217f566b6606
zhedongzheng <zhedongzheng@outlook.com>
2017-05-20 21:15:53 +0100

    more neat using a class generates next_batch

--- tensorflow-models/rnn/rnn_regr_plot.py
+++ tensorflow-models/rnn/rnn_regr_plot.py
@@ -4,50 +4,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-BATCH_START = 0     
-TIME_STEPS = 20     
-BATCH_SIZE = 50  
-
-
-def get_data_batch(return_x=False):
-    global BATCH_START, TIME_STEPS
-    xs = np.arange(BATCH_START, BATCH_START+TIME_STEPS*BATCH_SIZE).reshape((BATCH_SIZE, TIME_STEPS)) / (10*np.pi)
-    seq = np.sin(xs)
-    res = np.cos(xs)
-    BATCH_START += TIME_STEPS*BATCH_SIZE
-    if return_x:
-        return [seq[:, :, np.newaxis], res[:, :, np.newaxis], xs]
-    else:
-        return (seq[:, :, np.newaxis], res[:, :, np.newaxis])
+class TimeSeriesGen:
+    def __init__(self, batch_start=0, time_steps=20, batch_size=50):
+        self.start = batch_start
+        self.t_steps = time_steps
+        self.size = batch_size
+        
+    def next_batch(self):
+        xs = np.arange(self.start, self.start + self.t_steps * self.size)
+        xs = xs.reshape([self.size, self.t_steps]) / (10*np.pi)
+        self.start += self.t_steps * self.size
+        seq = np.sin(xs)
+        res = np.cos(xs)
+        return (seq[:, :, np.newaxis], res[:, :, np.newaxis], xs)
 
 
 if __name__ == '__main__':
-    data = [get_data_batch(return_x=True) for _ in range(2000)]
-    train_data = data[:1000]
-    test_data = data[1000:]
-    model = RNNRegressor(n_step = TIME_STEPS,
+    train_gen = TimeSeriesGen()
+    test_gen = TimeSeriesGen(1000)
+    model = RNNRegressor(n_step = 20,
                          n_in = 1,
                          n_out = 1,
                          cell_size = 16)
 
     model.sess.run(tf.global_variables_initializer())
-    plt.ion()
-    plt.show()
+    train_state = model.sess.run(model.init_state, feed_dict={model.batch_size:50})
+    test_state = model.sess.run(model.init_state, feed_dict={model.batch_size:50})
 
-    train_state = model.sess.run(model.init_state, {model.batch_size:BATCH_SIZE})
-    test_state = model.sess.run(model.init_state, {model.batch_size:BATCH_SIZE})
-    for train_idx, train_sample in enumerate(train_data):
-        seq, res, xs = train_sample
+    for _ in range(1000):
+        seq_train, res_train, _ = train_gen.next_batch()
         _, train_loss, train_state = model.sess.run([model.train_op, model.loss, model.final_state],
-                                                    {model.X:seq, model.Y:res, model.init_state:train_state,
-                                                     model.batch_size:BATCH_SIZE})
+                                                     feed_dict={model.X:seq_train, model.Y:res_train,
+                                                                model.init_state:train_state,
+                                                                model.batch_size:50})
 
-        test_sample = test_data[train_idx]
-        seq_test, res_test, xs_test = test_sample
+        seq_test, res_test, xs = test_gen.next_batch()
         test_loss, test_state, test_pred = model.sess.run([model.loss, model.final_state, model.time_seq_out],
                                                           {model.X:seq_test, model.Y:res_test,
                                                            model.init_state:test_state,
-                                                           model.batch_size:BATCH_SIZE})
+                                                           model.batch_size:50})
+
         
         # update plotting
         plt.plot(xs.ravel(), res_test.ravel(), 'r', xs.ravel(), test_pred.ravel(), 'b--')

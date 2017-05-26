260cbae489ec4dd9ea7bd0fe404046b5f16a7078
zhedongzheng <zhedongzheng@outlook.com>
2017-05-26 21:56:53 +0100

    replace test file

---
+++ tensorflow-models/rnn/rnn_text_gen_anna_test.py
@@ -0,0 +1,11 @@
+from rnn_text_gen import RNNTextGen
+
+
+prime_texts = ['i']
+
+
+if __name__ == '__main__':
+    with open('./temp/anna.txt') as f:
+        text = f.read()
+    model = RNNTextGen(text)
+    log = model.fit_text(prime_texts)

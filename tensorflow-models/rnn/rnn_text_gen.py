5847c8230790bed1d92d8e7a64367117498e38e3
zhedongzheng <zhedongzheng@outlook.com>
2017-06-01 19:08:37 +0800

    minor change

--- tensorflow-models/rnn/rnn_text_gen.py
+++ tensorflow-models/rnn/rnn_text_gen.py
@@ -134,13 +134,12 @@ class RNNTextGen:
     def preprocessing(self):
         text = self.text
         text = text.replace('\n', ' ')
-        if self.useless_words is None:
-            self.useless_words = string.punctuation
-        if sys.version[0] == 3:
-            table = str.maketrans( {useless: ' ' for useless in self.useless_words} )
-            text = text.translate(table)
-        else:
-            text = re.sub(r'[{}]'.format(''.join(self.useless_words)), ' ', text)
+        if self.useless_words is not None:
+            if sys.version[0] == 3:
+                table = str.maketrans({useless: ' ' for useless in self.useless_words})
+                text = text.translate(table)
+            else:
+                text = re.sub(r'[{}]'.format(''.join(self.useless_words)), ' ', text)
         text = re.sub('\s+', ' ', text ).strip().lower()
         
         chars = list(set(text))
--- tensorflow-models/rnn/rnn_text_gen_anna_test.py
+++ tensorflow-models/rnn/rnn_text_gen_anna_test.py
@@ -3,11 +3,10 @@ import string
 
 
 prime_texts = ['the']
-useless_words = [x for x in string.punctuation if x not in [',', "."]]
 
 
 if __name__ == '__main__':
     with open('./temp/anna.txt') as f:
         text = f.read()
-    model = RNNTextGen(text, useless_words=useless_words)
+    model = RNNTextGen(text)
     log = model.fit(prime_texts)

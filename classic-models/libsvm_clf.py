20eb4afc5ec23170a3a6172dfb90d027cf8bd3df
zhedongzheng <zhedongzheng@outlook.com>
2017-06-02 16:13:25 +0800

    minor change

--- classic-models/libsvm_clf.py
+++ classic-models/libsvm_clf.py
@@ -8,13 +8,16 @@ class SVC:
         self.param.kernel_type = libsvm.RBF
         self.param.C = C
         self.model = None
+
+
     def fit(self, X_train, y_train):
         X_train = np.array(X_train).tolist()
         y_train = np.array(y_train).tolist()
         problem = libsvm.svm_problem(y_train, X_train)
         self.model = libsvm.svm_train(problem, self.param)
+
+
     def predict(self, X_test):
         X_test = np.array(X_test).tolist()
         y_pred, _, _ = libsvm.svm_predict([0]*len(X_test), X_test, self.model)
         return y_pred
-

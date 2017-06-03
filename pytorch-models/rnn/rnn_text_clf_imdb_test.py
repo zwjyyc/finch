d1c9a3b622a0444c3f274d1d8620cc585b738c9d
zhedongzheng <zhedongzheng@outlook.com>
2017-06-03 15:41:14 +0800

    initial

---
+++ pytorch-models/rnn/rnn_text_clf_imdb_test.py
@@ -0,0 +1,17 @@
+from rnn_text_clf import RNNTextClassifier
+import tensorflow as tf
+
+
+vocab_size = 20000
+maxlen = 80
+n_epoch = 3
+
+
+if __name__ == '__main__':
+    (X_train, y_train), (X_test, y_test) = tf.contrib.keras.datasets.imdb.load_data('./imdb.npz', num_words=vocab_size)
+    X_train = tf.contrib.keras.preprocessing.sequence.pad_sequences(X_train, maxlen=maxlen)
+    X_test = tf.contrib.keras.preprocessing.sequence.pad_sequences(X_test, maxlen=maxlen)
+
+    clf = RNNTextClassifier(vocab_size)
+    clf.fit(X_train, y_train, n_epoch=n_epoch)
+    clf.evaluate(X_test, y_test)

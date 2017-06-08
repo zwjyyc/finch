0987e9ae1a58fefc36a782e2b11829df5a691067
zhedongzheng <zhedongzheng@outlook.com>
2017-06-08 10:47:54 +0800

    new models

---
+++ tensorflow-models/cnn/concat_conv_1d_text_clf.py
@@ -0,0 +1,220 @@
+import tensorflow as tf
+import numpy as np
+import math
+import sklearn
+
+
+class Conv1DClassifier:
+    def __init__(self, seq_len, vocab_size, n_out, sess=tf.Session(),
+                 embedding_dims=50, n_filters=250, kernel_size=3, padding='VALID'):
+        """
+        Parameters:
+        -----------
+        seq_len: int
+            Sequence length
+        vocab_size: int
+            Vocabulary size
+        embedding_dims: int
+            Word embedding dimensions
+        n_filters: int
+            Number output of filters in the convolution
+        kernel_size: int
+            Size of the 1D convolution window
+        hidden_dims: int
+            Ouput dimensions of the fully-connected layer
+        n_out: int
+            Output dimensions
+        sess: object
+            tf.Session() object 
+        """
+        self.seq_len = seq_len
+        self.vocab_size = vocab_size
+        self.embedding_dims = embedding_dims
+        self.n_filters = n_filters
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.n_out = n_out
+        self.sess = sess
+        self._cursor = None
+        self._seq_len = None
+        self.build_graph()
+    # end constructor
+ 
+ 
+    def build_graph(self):
+        self.add_input_layer()
+        self.add_word_embedding()
+        self.conv1 = self.add_conv1d('conv1', filter_shape=[self.kernel_size, self.embedding_dims, self.n_filters])
+        self.conv2 = self.add_conv1d('conv2', filter_shape=[self.kernel_size, self.embedding_dims, self.n_filters])
+        self.conv3 = self.add_conv1d('conv3', filter_shape=[self.kernel_size, self.embedding_dims, self.n_filters])
+        self.merge_layers([self.conv1, self.conv2, self.conv3])
+        self.add_global_pooling()
+        self.add_fc()
+        self.add_output_layer()   
+        self.add_backward_path()
+    # end method build_graph
+ 
+ 
+    def add_input_layer(self):
+        self.X = tf.placeholder(tf.int32, [None, self.seq_len])
+        self.Y = tf.placeholder(tf.float32, [None, self.n_out])
+        self.keep_prob = tf.placeholder(tf.float32)
+        self.lr = tf.placeholder(tf.float32)
+        self._cursor = self.X
+    # end method add_input_layer
+
+
+    def add_word_embedding(self):
+        E = tf.get_variable('E', [self.vocab_size,self.embedding_dims], tf.float32, tf.random_normal_initializer())
+        Y = tf.nn.embedding_lookup(E, self._cursor)
+        self._cursor = tf.nn.dropout(Y, self.keep_prob)
+    # end method add_word_embedding_layer
+
+
+    def add_conv1d(self, name, filter_shape, stride=1):
+        W = self.call_W(name+'_w', filter_shape)
+        b = self.call_b(name+'_b', [filter_shape[-1]])
+        Y = tf.nn.conv1d(self._cursor, W, stride=stride, padding=self.padding)
+        Y = tf.nn.bias_add(Y, b)
+        Y = tf.nn.relu(Y)
+        if self.padding == 'VALID':
+            self._seq_len = int(self.seq_len - self.kernel_size + 1 / stride)
+        if self.padding == 'SAME':
+            self._seq_len = int(self.seq_len / stride)
+        return Y
+    # end method add_conv1d_layer
+
+
+    def merge_layers(self, layers):
+        self._cursor = tf.concat(layers, axis=1)
+        self._seq_len = len(layers) * self._seq_len
+    # end method merge_layers
+
+
+    def add_global_pooling(self):
+        k = self._seq_len
+        Y = tf.expand_dims(self._cursor, 1)
+        Y = tf.nn.avg_pool(Y, ksize=[1,1,k,1], strides=[1,1,k,1], padding=self.padding)
+        Y = tf.squeeze(Y)
+        self._cursor = tf.reshape(Y, [-1, self.n_filters])
+    # end method add_global_maxpool_layer
+
+
+    def add_fc(self):
+        Y = tf.layers.dense(self._cursor, self.n_filters)
+        Y = tf.nn.dropout(Y, self.keep_prob)
+        self._cursor = tf.nn.relu(Y)
+    # end method add_fc
+
+
+    def add_output_layer(self):
+        self.logits = tf.layers.dense(self._cursor, self.n_out)
+    # end method add_output_layer
+
+
+    def add_backward_path(self):
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.Y))
+        self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+        self.acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.logits, 1),tf.argmax(self.Y, 1)), tf.float32))
+    # end method add_backward_path
+
+
+    def call_W(self, name, shape):
+        return tf.get_variable(name, shape, tf.float32, tf.contrib.layers.variance_scaling_initializer())
+    # end method _W
+
+
+    def call_b(self, name, shape):
+        return tf.get_variable(name, shape, tf.float32, tf.constant_initializer(0.01))
+    # end method _b
+
+
+    def fit(self, X, Y, val_data=None, n_epoch=10, batch_size=128, keep_prob=1.0, en_exp_decay=True,
+            en_shuffle=True):
+        if val_data is None:
+            print("Train %d samples" % len(X))
+        else:
+            print("Train %d samples | Test %d samples" % (len(X), len(val_data[0])))
+        log = {'loss':[], 'acc':[], 'val_loss':[], 'val_acc':[]}
+        global_step = 0
+
+        self.sess.run(tf.global_variables_initializer()) # initialize all variables
+        for epoch in range(n_epoch):
+            if en_shuffle:
+                X, Y = sklearn.utils.shuffle(X, Y)
+            local_step = 1
+            
+            for X_batch, Y_batch in zip(self.gen_batch(X, batch_size),
+                                        self.gen_batch(Y, batch_size)): # batch training
+                lr = self.decrease_lr(en_exp_decay, global_step, n_epoch, len(X), batch_size) 
+                _, loss, acc = self.sess.run([self.train_op, self.loss, self.acc],
+                                             {self.X:X_batch, self.Y:Y_batch,
+                                              self.lr:lr, self.keep_prob:keep_prob})
+                local_step += 1
+                global_step += 1
+                if local_step % 50 == 0:
+                    print ("Epoch %d/%d | Step %d/%d | train_loss: %.4f | train_acc: %.4f | lr: %.4f"
+                        %(epoch+1, n_epoch, local_step, int(len(X)/batch_size), loss, acc, lr))
+
+            if val_data is not None: # go through test dara, compute averaged validation loss and acc
+                val_loss_list, val_acc_list = [], []
+                for X_test_batch, Y_test_batch in zip(self.gen_batch(val_data[0], batch_size),
+                                                      self.gen_batch(val_data[1], batch_size)):
+                    v_loss, v_acc = self.sess.run([self.loss, self.acc],
+                                                  {self.X:X_test_batch, self.Y:Y_test_batch,
+                                                   self.keep_prob:1.0})
+                    val_loss_list.append(v_loss)
+                    val_acc_list.append(v_acc)
+                val_loss, val_acc = self.list_avg(val_loss_list), self.list_avg(val_acc_list)
+
+            # append to log
+            log['loss'].append(loss)
+            log['acc'].append(acc)
+            if val_data is not None:
+                log['val_loss'].append(val_loss)
+                log['val_acc'].append(val_acc)
+            # verbose
+            if val_data is None:
+                print ("Epoch %d/%d | train_loss: %.4f | train_acc: %.4f |" % (epoch+1, n_epoch, loss, acc),
+                    "lr: %.4f" % (lr) )
+            else:
+                print ("Epoch %d/%d | train_loss: %.4f | train_acc: %.4f |" % (epoch+1, n_epoch, loss, acc),
+                    "test_loss: %.4f | test_acc: %.4f |" % (val_loss, val_acc),
+                    "lr: %.4f" % (lr) )
+        # end "for epoch in range(n_epoch):"
+
+        return log
+    # end method fit
+
+
+    def predict(self, X_test, batch_size=128):
+        batch_pred_list = []
+        for X_test_batch in self.gen_batch(X_test, batch_size):
+            batch_pred = self.sess.run(self.logits, {self.X:X_test_batch, self.keep_prob:1.0})
+            batch_pred_list.append(batch_pred)
+        return np.vstack(batch_pred_list)
+    # end method predict
+
+
+    def gen_batch(self, arr, batch_size):
+        for i in range(0, len(arr), batch_size):
+            yield arr[i : i+batch_size]
+    # end method gen_batch
+
+
+    def decrease_lr(self, en_exp_decay, global_step, n_epoch, len_X, batch_size):
+        if en_exp_decay:
+            max_lr = 0.005
+            min_lr = 0.001
+            decay_rate = math.log(min_lr/max_lr) / (-n_epoch*len_X/batch_size)
+            lr = max_lr*math.exp(-decay_rate*global_step)
+        else:
+            lr = 0.005
+        return lr
+    # end method adjust_lr
+
+
+    def list_avg(self, l):
+        return sum(l) / len(l)
+    # end method list_avg
+# end class
---
+++ tensorflow-models/cnn/concat_conv_1d_text_clf_imdb_test.py
@@ -0,0 +1,27 @@
+from concat_conv_1d_text_clf import Conv1DClassifier
+import tensorflow as tf
+import numpy as np
+
+
+max_features = 5000
+maxlen = 400  # cut texts after this number of words (among top max_features most common words)
+n_out = 2
+
+
+if __name__ == '__main__':
+    (X_train, y_train), (X_test, y_test) = tf.contrib.keras.datasets.imdb.load_data(num_words=max_features)
+
+    X_train = tf.contrib.keras.preprocessing.sequence.pad_sequences(X_train, maxlen=maxlen)
+    X_test = tf.contrib.keras.preprocessing.sequence.pad_sequences(X_test, maxlen=maxlen)
+    print('X_train shape:', X_train.shape, 'X_test shape:', X_test.shape)
+    
+    Y_train = tf.contrib.keras.utils.to_categorical(y_train)
+    Y_test = tf.contrib.keras.utils.to_categorical(y_test)
+
+    clf = Conv1DClassifier(maxlen, max_features, n_out)
+    log = clf.fit(X_train, Y_train, n_epoch=3, batch_size=32, keep_prob=0.8, en_exp_decay=True,
+                  val_data=(X_test,Y_test))
+    pred = clf.predict(X_test)
+
+    final_acc = (np.argmax(pred,1) == np.argmax(Y_test,1)).astype(float).mean()
+    print("final testing accuracy: %.4f" % final_acc)

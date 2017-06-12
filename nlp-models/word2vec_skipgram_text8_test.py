9e3ccc90393fd411a2cc63cf0773785db10ff062
zhedongzheng <zhedongzheng@outlook.com>
2017-06-12 19:01:10 +0800

    change sample words to view results

--- nlp-models/word2vec_skipgram_text8_test.py
+++ nlp-models/word2vec_skipgram_text8_test.py
@@ -5,7 +5,7 @@ from word2vec_skipgram import SkipGram
 if __name__ == '__main__':
     with open('temp/text8.txt') as f:
         text = f.read()
-    sample_words = ['king', 'zero', 'one', 'america']
+    sample_words = ['one', 'game', 'american', 'college']
 
     model = SkipGram(text, sample_words, useless_words=string.punctuation)
     model.fit()

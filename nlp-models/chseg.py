f7da3a584df66a57df79d26b1cfa41531e359a25
zhedongzheng <zhedongzheng@outlook.com>
2017-06-15 22:38:59 +0800

    update

--- nlp-models/chseg.py
+++ nlp-models/chseg.py
@@ -1,14 +1,18 @@
 import re
+import sys
+import jieba
 
 
-def build_data():
+def load_data():
     # B: 0, M: 1, E: 2, S: 3
     char2idx = {}
+    idx2char = {}
     char_idx = 0
     x_train = []
     y_train = []
     x_test = []
     y_test = []
+    version = int(sys.version[0])
 
     def preprocess(path):
         text = open(path).read()
@@ -26,25 +30,38 @@ def build_data():
                 ys.append(2)
             else:
                 ys.append(1)
-
-    text_train = preprocess('temp/icwb2-data/training/pku_training.utf8')
+    
+    if version == 3:
+        text_train = preprocess('temp/icwb2-data/training/pku_training.txt')
+        text_train += preprocess('temp/icwb2-data/training/msr_training.txt')
+    else:
+        text_train = preprocess('temp/icwb2-data/training/pku_training.utf8')
+        text_train += preprocess('temp/icwb2-data/training/msr_training.utf8')
     segs_train = text_train.split()
-    text_test = preprocess('temp/icwb2-data/testing/pku_test.utf8')
-    segs_test = text_test.split()
+    if version == 3:
+        text_test = preprocess('temp/icwb2-data/testing/pku_test.txt')
+        text_test += preprocess('temp/icwb2-data/testing/msr_test.txt')
+    else:
+        text_test = preprocess('temp/icwb2-data/testing/pku_test.utf8')
+        text_test += preprocess('temp/icwb2-data/testing/msr_test.utf8')
+    segs_test = jieba.cut(text_test)
 
     for seg in segs_train:
-        chars = list(seg.decode('utf-8'))
+        chars = list(seg) if version == 3 else list(seg.decode('utf-8'))
         for i, char in enumerate(chars):
             # handle x
             if char not in char2idx:
                 char2idx[char] = char_idx
+                idx2char[char_idx] = char
                 char_idx += 1
             x_train.append(char2idx[char])
             # handle y
             build_y(chars, y_train)
 
+    char2idx['_unknown'] = char_idx
+
     for seg in segs_test:
-        chars = list(seg.decode('utf-8'))
+        chars = list(seg) if version == 3 else list(seg.decode('utf-8'))
         for i, char in enumerate(chars):
             # handle x
             if char in char2idx:
@@ -53,5 +70,5 @@ def build_data():
                 x_test.append(char_idx)
             # handle y
             build_y(chars, y_test)
-
-    return x_train, y_train, x_test, y_test, len(char2idx), char2idx
+    
+    return x_train, y_train, x_test, y_test, len(char2idx), char2idx, idx2char
---
+++ nlp-models/chseg_rnn_test.py
@@ -0,0 +1,51 @@
+import chseg
+import numpy as np
+import tensorflow as tf
+from rnn_seq2seq_clf import RNNTextClassifier
+from collections import Counter
+
+
+SEQ_LEN = 50
+N_CLASS = 4
+sample = '没有孩子的世界是寂寞的'
+
+
+def x_to_seq(*args):
+    data = []
+    for x in args:
+        x = x[: (len(x) - len(x) % SEQ_LEN)]
+        data.append(np.reshape(x, [-1, SEQ_LEN]))
+    return data
+
+
+def y_to_seq(*args):
+    data = []
+    for y in args:
+        y = y[: (len(y) - len(y) % SEQ_LEN)]
+        y = tf.contrib.keras.utils.to_categorical(y)
+        data.append(np.reshape(y, [-1, SEQ_LEN, N_CLASS]))
+    return data
+
+
+if __name__ == '__main__':
+    x_train, y_train, x_test, y_test, vocab_size, char2idx, idx2char = chseg.load_data()
+    X_train, X_test = x_to_seq(x_train, x_test)
+    Y_train, Y_test = y_to_seq(y_train, y_test)
+    print('Vocab size: %d' % vocab_size)
+
+    clf = RNNTextClassifier(SEQ_LEN, vocab_size, N_CLASS)
+    clf.fit(X_train, Y_train, val_data=(X_test, Y_test), rnn_keep_prob=0.5, n_epoch=1)
+    Y_pred = clf.predict(X_test)
+    Y_test = Y_test.reshape(-1, N_CLASS)
+    final_acc = (np.argmax(Y_pred,1) == np.argmax(Y_test,1)).mean()
+    print("final testing accuracy: %.4f" % final_acc)
+    
+    preds = clf.infer([char2idx[c] for c in list(sample)])
+    labels = np.argmax(preds, 1)
+    res = ''
+    for i, l in enumerate(labels):
+        c = sample[i]
+        if l == 2 or l == 3:
+            c += ' '
+        res += c
+    print(res)

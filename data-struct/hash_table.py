f43551df1571895ce3620af454f456ba48a615b5
zhedongzheng <zhedongzheng@outlook.com>
2017-06-19 18:19:03 +0800

    update

--- data-struct/hash_table.py
+++ data-struct/hash_table.py
@@ -1,44 +1,52 @@
-class Entry:
-    def __init__(self, key=None, val=None):
+class Node:
+    def __init__(self, key, val, next):
         self.key = key
         self.val = val
-        self.next = None
+        self.next = next
 
-    def get_key(self):
-        return self.key
 
-    def get_val(self):
-        return self.val
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, key, val):
+        new_node = Node(key, val, None)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
 
 class HashTable:
     def __init__(self, size):
         self.size = size
-        self.hash_array = [Entry() for _ in range(self.size)]
+        self.hash_array = [LinkedList() for _ in range(self.size)]
 
     def get_hash(self, key):
         return key % self.size
 
     def put(self, key, val):
         hash_index = self.get_hash(key)
-        hashed = self.hash_array[hash_index]
-        new_item = Entry(key, val)
-        new_item.next = hashed
-        hashed.next = new_item
+        linked_list = self.hash_array[hash_index]
+        new_item = linked_list.add(key, val)
 
     def get(self, key):
         val = None
         hash_index = self.get_hash(key)
-        hashed = self.hash_array[hash_index]
-        while hashed.next is not None:
-            if hashed.get_key() == key:
-                val = hashed.get_val()
+        node = self.hash_array[hash_index].head
+        while node is not None:
+            if node.key == key:
+                val = node.val 
                 break
-            hashed = hashed.next
+            node = node.next
         return val
+
 
 if __name__ == '__main__':
     ht = HashTable(10)
     ht.put(11, 'pd')
     ht.put(12, 'ax')
     ht.put(13, 'jb')
-    print(ht.get(12))
+    ht.put(21, 'ab')
+    print(ht.get(21))

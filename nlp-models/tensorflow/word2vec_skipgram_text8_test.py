a07d4ec4d9d6cb1471105f72f4b243e988de9e08
zhedongzheng <zhedongzheng@outlook.com>
2017-06-28 23:02:49 +0800

    update

--- nlp-models/tensorflow/word2vec_skipgram_text8_test.py
+++ nlp-models/tensorflow/word2vec_skipgram_text8_test.py
@@ -5,7 +5,7 @@ from word2vec_skipgram import SkipGram
 if __name__ == '__main__':
     with open('temp/text8.txt') as f:
         text = f.read()
-    sample_words = ['one', 'game', 'american', 'college']
+    sample_words = ['one', 'gold', 'american', 'college']
 
     model = SkipGram(text, sample_words, skip_window=10, useless_words=string.punctuation)
     model.fit()
--- tensorflow-models/linear_model/logistic.py
+++ tensorflow-models/linear_model/logistic.py
@@ -37,7 +37,8 @@ class Logistic:
 
 
     def add_output_layer(self):
-        self.W = tf.get_variable('W', [self.n_in, self.n_out], tf.float32, tf.random_normal_initializer())
+        self.W = tf.get_variable('W', [self.n_in, self.n_out], tf.float32,
+                                  tf.contrib.layers.variance_scaling_initializer())
         self.b = tf.get_variable('b', [self.n_out], tf.float32, tf.constant_initializer(0.01))
         self.logits = tf.nn.bias_add(tf.matmul(self.X, self.W), self.b)
     # end method add_output_layer

4acc1fb1b76ae4148c9a6ae5433b9cac80d6da4a
zhedongzheng <zhedongzheng@outlook.com>
2017-06-30 18:38:30 +0800

    update

--- nlp-models/tensorflow/chseg_birnn_test.py
+++ nlp-models/tensorflow/chseg_birnn_test.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+Vocab size: 4532
+Train 29240 samples | Test 7288 samples
+Epoch 1/1 | Step 0/228 | train_loss: 1.3943 | train_acc: 0.2592 | lr: 0.0050
+Epoch 1/1 | Step 50/228 | train_loss: 0.4623 | train_acc: 0.8291 | lr: 0.0030
+Epoch 1/1 | Step 100/228 | train_loss: 0.3521 | train_acc: 0.8770 | lr: 0.0018
+Epoch 1/1 | Step 150/228 | train_loss: 0.3412 | train_acc: 0.8789 | lr: 0.0011
+Epoch 1/1 | Step 200/228 | train_loss: 0.2663 | train_acc: 0.9031 | lr: 0.0007
+Epoch 1/1 | train_loss: 0.3519 | train_acc: 0.8746 | test_loss: 0.3390 | test_acc: 0.8794 | lr: 0.0005
+我 来到 大学 读书 ， 希望 学到 知识 
+"""
 import sys
 import chseg
 import numpy as np

497f32c11a5aa42118ec6274e49b722dd800aaa9
zhedongzheng <zhedongzheng@outlook.com>
2017-07-16 20:23:43 +0800

    clean code

--- tensorflow-models/cnn/conv_2d_clf_cifar10_keras_idg_test.py
+++ tensorflow-models/cnn/conv_2d_clf_cifar10_keras_idg_test.py
@@ -1,10 +1,9 @@
 from conv_2d_clf import Conv2DClassifier
-import numpy as np
 import tensorflow as tf
 
 
-batch_size = 128
-n_epoch = 20
+BATCH_SIZE = 128
+N_EPOCH = 10
 
 
 if __name__ == '__main__':
@@ -14,48 +13,34 @@ if __name__ == '__main__':
 
     model = Conv2DClassifier((32,32), 3, 10)
 
-    datagen = tf.contrib.keras.preprocessing.image.ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip=True,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
+    datagen = tf.contrib.keras.preprocessing.image.ImageDataGenerator()
     datagen.fit(X_train)
 
     global_step = 0
-    model.sess.run(tf.global_variables_initializer()) # initialize all variables
-    for epoch in range(n_epoch):
-
-        local_step = 1
-        for X_batch, Y_batch in datagen.flow(X_train, y_train, batch_size=batch_size):
-            if local_step > int(len(X_train)/batch_size):
+    model.sess.run(tf.global_variables_initializer())
+    for epoch in range(N_EPOCH):
+        for local_step, (X_batch, Y_batch) in enumerate(datagen.flow(X_train, y_train, batch_size=BATCH_SIZE)):
+            if local_step > len(X_train) // BATCH_SIZE:
                 break
-            lr = model.decrease_lr(True, global_step, n_epoch, len(X_train), batch_size) 
+            lr = model.decrease_lr(True, global_step, N_EPOCH, len(X_train), BATCH_SIZE) 
             _, loss, acc = model.sess.run([model.train_op, model.loss, model.acc],
-                                          feed_dict={model.X:X_batch, model.Y:Y_batch.squeeze(),
-                                                     model.lr:lr, model.keep_prob:0.5,
-                                                     model.train_flag:True})
-            local_step += 1
+                                          {model.X:X_batch, model.Y:Y_batch.squeeze(),
+                                           model.lr:lr, model.keep_prob:0.5, model.train_flag:True})
             global_step += 1
             if local_step % 50 == 0:
                 print ("Epoch %d/%d | Step %d/%d | train_loss: %.4f | train_acc: %.4f | lr: %.4f"
-                        %(epoch+1, n_epoch, local_step, int(len(X_train)/batch_size), loss, acc, lr))
+                        %(epoch+1, N_EPOCH, local_step, len(X_train)//BATCH_SIZE, loss, acc, lr))
 
         val_loss_list, val_acc_list = [], []
-        for X_test_batch, Y_test_batch in zip(model.gen_batch(X_test, batch_size),
-                                              model.gen_batch(y_test, batch_size)):
+        for X_test_batch, Y_test_batch in zip(model.gen_batch(X_test, BATCH_SIZE),
+                                              model.gen_batch(y_test, BATCH_SIZE)):
             v_loss, v_acc = model.sess.run([model.loss, model.acc],
-                                            feed_dict={model.X:X_test_batch, model.Y:Y_test_batch.squeeze(),
-                                                       model.keep_prob:1.0, model.train_flag:False})
+                                           {model.X:X_test_batch, model.Y:Y_test_batch.squeeze(),
+                                            model.keep_prob:1.0, model.train_flag:False})
             val_loss_list.append(v_loss)
             val_acc_list.append(v_acc)
         val_loss, val_acc = model.list_avg(val_loss_list), model.list_avg(val_acc_list)
 
-        print ("Epoch %d/%d | train_loss: %.4f | train_acc: %.4f |" % (epoch+1, n_epoch, loss, acc),
+        print ("Epoch %d/%d | train_loss: %.4f | train_acc: %.4f |" % (epoch+1, N_EPOCH, loss, acc),
                 "test_loss: %.4f | test_acc: %.4f |" % (val_loss, val_acc),
                 "lr: %.4f" % (lr) )

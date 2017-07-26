74dcc1a63553a89aadb2f85fe59a61a8a267a8d9
zhedongzheng <zhedongzheng@outlook.com>
2017-07-26 10:51:24 +0800

    update

---
+++ tensorflow-models/autoencoder/denoising_ae_mnist_test.py
@@ -0,0 +1,21 @@
+from denoising_ae import Autoencoder
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+if __name__ == '__main__':
+    (X_train, y_train), (X_test, y_test) = tf.contrib.keras.datasets.mnist.load_data()
+    X_train = (X_train/255.0).reshape(-1, 28*28)
+    X_test = (X_test/255.0).reshape(-1, 28*28)
+
+    auto = Autoencoder(28*28, [256,128,32,2])
+    auto.fit(X_train, val_data=X_test)
+    X_test_2d = auto.transform(X_test)
+    X_test_pred = auto.predict(X_test)
+    
+    plt.scatter(X_test_2d[:, 0], X_test_2d[:, 1], c=y_test)
+    plt.show()
+    plt.imshow(X_test[21].reshape(28,28))
+    plt.show()
+    plt.imshow(X_test_pred[21].reshape(28,28))
+    plt.show()

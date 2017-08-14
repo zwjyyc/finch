46a6f16130b43535296c4fbdfa75089b68bdc7f2
zhedongzheng <zhedongzheng@outlook.com>
2017-08-14 15:49:12 +0800

    style change

--- nlp-models/tensorflow/seq2seq_ultimate.py
+++ nlp-models/tensorflow/seq2seq_ultimate.py
@@ -27,13 +27,11 @@ class Seq2Seq:
         self.add_input_layer()
         self.add_encoder_layer()
         with tf.variable_scope('decode'):
-            self.add_attention_for_training()
             self.add_decoder_for_training()
         with tf.variable_scope('decode', reuse=True):
-            self.add_attention_for_predicting()
-            self.add_decoder_for_predicting()
+            self.add_decoder_for_inference()
         self.add_backward_path()
-    # end method build_graph
+    # end method
 
 
     def add_input_layer(self):
@@ -41,12 +39,12 @@ class Seq2Seq:
         self.Y = tf.placeholder(tf.int32, [self.batch_size, None])
         self.X_seq_len = tf.placeholder(tf.int32, [self.batch_size])
         self.Y_seq_len = tf.placeholder(tf.int32, [self.batch_size])
-    # end method add_input_layer
+    # end method
 
 
     def lstm_cell(self, reuse=False):
         return tf.nn.rnn_cell.LSTMCell(self.rnn_size, initializer=tf.orthogonal_initializer(), reuse=reuse)
-    # end method lstm_cell
+    # end method
 
 
     def add_encoder_layer(self):
@@ -63,14 +61,14 @@ class Seq2Seq:
                 scope = 'bidirectional_rnn_'+str(n))
             self.encoder_out = tf.concat((out_fw, out_bw), 2)
             self.encoder_state += (state_fw, state_bw)
-    # end method add_encoder_layer
+    # end method
     
 
     def processed_decoder_input(self):
         main = tf.strided_slice(self.Y, [0, 0], [self.batch_size, -1], [1, 1]) # remove last char
         decoder_input = tf.concat([tf.fill([self.batch_size, 1], self._y_go), main], 1)
         return decoder_input
-    # end method add_decoder_layer
+    # end method
 
 
     def add_attention_for_training(self):
@@ -82,12 +80,14 @@ class Seq2Seq:
             cell = tf.nn.rnn_cell.MultiRNNCell([self.lstm_cell() for _ in range(2 * self.n_layers)]),
             attention_mechanism = attention_mechanism,
             attention_layer_size = self.rnn_size)
-    # end method add_attention
+    # end method
 
 
     def add_decoder_for_training(self):
+        self.add_attention_for_training()
         decoder_embedding = tf.get_variable('decoder_embedding', [len(self.Y_word2idx), self.decoder_embedding_dim],
                                              tf.float32, tf.random_uniform_initializer(-1.0, 1.0))
+        
         training_helper = tf.contrib.seq2seq.TrainingHelper(
             inputs = tf.nn.embedding_lookup(decoder_embedding, self.processed_decoder_input()),
             sequence_length = self.Y_seq_len,
@@ -102,10 +102,10 @@ class Seq2Seq:
             impute_finished = True,
             maximum_iterations = tf.reduce_max(self.Y_seq_len))
         self.training_logits = training_decoder_output.rnn_output
-    # end method add_decoder_layer
+    # end method
 
 
-    def add_attention_for_predicting(self):
+    def add_attention_for_inference(self):
         self.encoder_out_tiled = tf.contrib.seq2seq.tile_batch(self.encoder_out, self.beam_width)
         self.encoder_state_tiled = tf.contrib.seq2seq.tile_batch(self.encoder_state, self.beam_width)
         self.X_seq_len_tiled = tf.contrib.seq2seq.tile_batch(self.X_seq_len, self.beam_width)
@@ -118,10 +118,12 @@ class Seq2Seq:
             cell = tf.nn.rnn_cell.MultiRNNCell([self.lstm_cell(reuse=True) for _ in range(2 * self.n_layers)]),
             attention_mechanism = attention_mechanism,
             attention_layer_size = self.rnn_size)
-    # end method add_attention_for_predicting
+    # end method
 
 
-    def add_decoder_for_predicting(self):
+    def add_decoder_for_inference(self):
+        self.add_attention_for_inference()
+
         predicting_decoder = tf.contrib.seq2seq.BeamSearchDecoder(
             cell = self.decoder_cell,
             embedding = tf.get_variable('decoder_embedding'),
@@ -137,7 +139,7 @@ class Seq2Seq:
             impute_finished = False,
             maximum_iterations = 2 * tf.reduce_max(self.X_seq_len))
         self.predicting_logits = predicting_decoder_output.predicted_ids[:, :, 0]
-    # end method add_decoder_for_predicting
+    # end method
 
 
     def add_backward_path(self):
@@ -145,13 +147,12 @@ class Seq2Seq:
         self.loss = tf.contrib.seq2seq.sequence_loss(logits = self.training_logits,
                                                      targets = self.Y,
                                                      weights = masks)
-
         # gradient clipping
         params = tf.trainable_variables()
         gradients = tf.gradients(self.loss, params)
         clipped_gradients, _ = tf.clip_by_global_norm(gradients, self.grad_clip)
         self.train_op = tf.train.AdamOptimizer().apply_gradients(zip(clipped_gradients, params))
-    # end method add_backward_path
+    # end method
 
 
     def pad_sentence_batch(self, sentence_batch, pad_int):
@@ -162,7 +163,7 @@ class Seq2Seq:
             padded_seqs.append(sentence + [pad_int] * (max_sentence_len - len(sentence)))
             seq_lens.append(len(sentence))
         return padded_seqs, seq_lens
-    # end method pad_sentence_batch
+    # end method
 
 
     def next_batch(self, X, Y, X_pad_int=None, Y_pad_int=None):
@@ -180,7 +181,7 @@ class Seq2Seq:
                    np.array(padded_Y_batch),
                    X_batch_lens,
                    Y_batch_lens)
-    # end method gen_batch
+    # end method
 
 
     def fit(self, X_train, Y_train, val_data, n_epoch=60, display_step=50):
@@ -203,7 +204,7 @@ class Seq2Seq:
                                                          self.Y_seq_len: Y_test_batch_lens})
                     print("Epoch %d/%d | Batch %d/%d | train_loss: %.3f | test_loss: %.3f"
                         % (epoch, n_epoch, local_step, len(X_train)//self.batch_size, loss, val_loss))
-    # end method fit
+    # end method
 
 
     def infer(self, input_word, X_idx2word, Y_idx2word):        
@@ -219,7 +220,7 @@ class Seq2Seq:
         print('\nTarget')
         print('Word: {}'.format([i for i in out_indices]))
         print('OUT: {}'.format(' '.join([Y_idx2word[i] for i in out_indices])))
-    # end method infer
+    # end method
 
 
     def register_symbols(self):
@@ -232,5 +233,5 @@ class Seq2Seq:
         self._y_eos = self.Y_word2idx['<EOS>']
         self._y_pad = self.Y_word2idx['<PAD>']
         self._y_unk = self.Y_word2idx['<UNK>']
-    # end method add_symbols
+    # end method
 # end class

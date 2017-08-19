0039404c1feaad061272dac5716fe81fa73d3ac1
zhedongzheng <zhedongzheng@outlook.com>
2017-08-19 19:54:27 +0800

    function -> model

--- ir-models/python/apriori.py
+++ ir-models/python/apriori.py
@@ -2,31 +2,40 @@ from __future__ import division
 import itertools
 
 
-def support(items, table):
-    reduced_table = table
-    reduced_users = table.index
-    for item in items:
-        reduced_table = reduced_table.loc[reduced_table.loc[:, item] > 0]
-        reduced_users = reduced_table.index
-    return len(reduced_users) / len(table.index)
+class Apriori:
+    def __init__(self, min_support=0.3, min_confidence=0.1, n_item_rule=2):
+        self.min_support = min_support
+        self.min_confidence = min_confidence
+        self.n_item_rule = n_item_rule
+        self.assoc_rules = None
+
+    def _support(self, items, table):
+        reduced_table = table
+        reduced_users = table.index
+        for item in items:
+            reduced_table = reduced_table.loc[reduced_table.loc[:, item] > 0]
+            reduced_users = reduced_table.index
+        return len(reduced_users) / len(table.index)
 
 
-def apriori(table, min_support=0.3, min_confidence=0.1, n_item_rule=2):
-    reduced_items = []
-    for item in list(table.columns):
-        if support([item], table) > min_support:
-            reduced_items.append(item)
-    
-    assoc_rules = []
-    for rule in itertools.permutations(reduced_items, n_item_rule):
-        from_item, to_item = rule[0], rule
-        support_to_item = support(to_item, table)
-        confidence = support_to_item / support([from_item], table)
-        if confidence > min_confidence and support_to_item > min_support:
-            assoc_rules.append(rule)
-    
-    for items in assoc_rules:
-        out = "who is watching %s is likely to watch" % items[0]
-        for i in range(1, n_item_rule):
-            out += ' %s' % items[i]
-        print(out)
+    def fit(self, table):
+        reduced_items = []
+        for item in list(table.columns):
+            if self._support([item], table) > self.min_support:
+                reduced_items.append(item)
+        
+        self.assoc_rules = []
+        for rule in itertools.permutations(reduced_items, self.n_item_rule):
+            from_item, to_item = rule[0], rule
+            support_to_item = self._support(to_item, table)
+            confidence = support_to_item / self._support([from_item], table)
+            if confidence > self.min_confidence and support_to_item > self.min_support:
+                self.assoc_rules.append(rule)
+
+
+    def predict(self):
+        for items in self.assoc_rules:
+            out = "who is watching %s is likely to watch" % items[0]
+            for i in range(1, self.n_item_rule):
+                out += ' %s' % items[i]
+            print(out)
--- ir-models/python/apriori_movielens_test.py
+++ ir-models/python/apriori_movielens_test.py
@@ -1,11 +1,13 @@
-from apriori import apriori
+from apriori import Apriori
 import pandas as pd
 
 
 def main():
     df = pd.read_csv('./temp/ml-100k/u.data', sep='\t', header=None, usecols=[0, 1, 2], names=['userid', 'itemid', 'rating'])
     table = pd.pivot_table(df, values='rating', index=['userid'], columns=['itemid'])
-    apriori(table)
+    model = Apriori()
+    model.fit(table)
+    model.predict()
 
 
 if __name__ == '__main__':

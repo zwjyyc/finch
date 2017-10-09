46f5e63eb60bf2053f6de36010b1897bab8c2e14
Zhedong Zheng <zhedongzheng@Zhedongs-MacBook-Pro.local>
2017-10-09 21:40:51 +0100

    clean code a bit

--- nlp-models/tensorflow/cnn_rnn_text_gen.py
+++ nlp-models/tensorflow/cnn_rnn_text_gen.py
@@ -1,30 +1,11 @@
 import tensorflow as tf
 import numpy as np
-import math
 import sys
 
 
 class ConvRNNTextGen:
     def __init__(self, text, seq_len=50, embedding_dims=15, cell_size=128, n_layer=2, grad_clip=5.0,
                  n_filters=[8, 16, 32, 64, 128], kernel_sizes=[1, 2, 3, 4, 5], sess=tf.Session()):
-        """
-        Parameters:
-        -----------
-        sess: object
-            tf.Session() object
-        text: string
-            corpus in one long string, usually obtained by file.read()
-        seq_len: int
-            Sequence length
-        embedding_dims: int
-            length of embedding vector for each word
-        cell_size: int
-            Number of units in the rnn cell, default to 128
-        n_layers: int
-            Number of layers of stacked rnn cells
-        useless_words: list of characters
-            all the useless_words which will be removed from text, usually punctuations
-        """
         self.sess = sess
         self.text = text
         self.seq_len = seq_len
@@ -53,16 +34,15 @@ class ConvRNNTextGen:
         self.add_dynamic_rnn()
         self.add_output_layer()
         self.add_backward_path()
-    # end method build_graph
+    # end method
 
 
     def add_input_layer(self):
         self.X = tf.placeholder(tf.int32, [None, None, self.max_word_len])
         self.Y = tf.placeholder(tf.int32, [None, None])
-        self.lr = tf.placeholder(tf.float32)
         self.batch_size = tf.shape(self.X)[0]
         self._pointer = self.X
-    # end method add_input_layer
+    # end method
 
 
     def add_word_embedding(self):
@@ -70,7 +50,7 @@ class ConvRNNTextGen:
         embedding = tf.get_variable('encoder', [self.vocab_char, self.embedding_dims], tf.float32,
                                      tf.random_uniform_initializer(-1.0, 1.0))
         self._pointer = tf.nn.embedding_lookup(embedding, self._pointer)
-    # end method add_word_embedding
+    # end method
 
 
     def add_concat_conv(self):
@@ -92,7 +72,7 @@ class ConvRNNTextGen:
                                                padding = 'valid')
             parallels.append(tf.reshape(pool_out, [self.batch_size, -1, n_filter])) # [batch_size, seq_len, n_filter]
         self._pointer = tf.concat(parallels, 2)
-    # end method add_concat_conv
+    # end method
 
 
     def add_highway(self, i):
@@ -105,7 +85,7 @@ class ConvRNNTextGen:
         highway_out = tf.add(tf.multiply(H, T), tf.multiply(reshaped, C))
         
         self._pointer = tf.reshape(highway_out, [self.batch_size, -1, size]) # [batch_size, seq_len, size]
-    # end method add_highway
+    # end method
 
 
     def add_lstm_cells(self):
@@ -113,20 +93,20 @@ class ConvRNNTextGen:
             cell = tf.nn.rnn_cell.LSTMCell(self.cell_size, initializer=tf.orthogonal_initializer())
             return cell
         self.cells = tf.nn.rnn_cell.MultiRNNCell([cell() for _ in range(self.n_layer)])
-    # end method add_rnn_cells
+    # end method
 
 
     def add_dynamic_rnn(self):
         self.init_state = self.cells.zero_state(self.batch_size, tf.float32)
         self._pointer, self.final_state = tf.nn.dynamic_rnn(self.cells, self._pointer, initial_state=self.init_state)   
-    # end method add_dynamic_rnn
+    # end method
 
 
     def add_output_layer(self):
         reshaped = tf.reshape(self._pointer, [-1, self.cell_size])
         self.logits = tf.layers.dense(reshaped, self.vocab_word, name='output')
         self.softmax_out = tf.nn.softmax(self.logits)
-    # end method add_output_layer
+    # end method
 
 
     def add_backward_path(self):
@@ -140,55 +120,8 @@ class ConvRNNTextGen:
         params = tf.trainable_variables()
         gradients = tf.gradients(self.loss, params)
         clipped_gradients, _ = tf.clip_by_global_norm(gradients, self.grad_clip)
-        self.train_op = tf.train.AdamOptimizer(self.lr).apply_gradients(zip(clipped_gradients, params))
-    # end method add_backward_path
-
-
-    def adjust_lr(self, current_step, total_steps):
-        max_lr = 0.005
-        min_lr = 0.001
-        decay_rate = math.log(min_lr/max_lr) / (-total_steps)
-        lr = max_lr * math.exp(-decay_rate * current_step)
-        return lr
-    # end method adjust_lr
-
-
-    def preprocessing(self):
-        text = self.text        
-        chars = set(text)
-        self.char2idx = {c: i+1 for i, c in enumerate(chars)}
-        self.char2idx['<PAD>'] = 0
-        self.idx2char = {i : c for c, i in self.char2idx.items()}
-        self.vocab_char = len(self.idx2char)
-        print("Vocabulary of Char:", self.vocab_char)
-
-        text = text.replace('\n', ' \n ')
-        text = text.replace('  ', ' ')
-        text = text.lower()
-        tokens = text.split(' ')
-        # print(tokens)
-
-        words = set(tokens)
-        self.max_word_len = max([len(w) for w in words])
-        self.word2idx = {w: i for i, w in enumerate(words)}
-        self.idx2word = {i: w for i, w in enumerate(words)}
-        self.vocab_word = len(self.word2idx)
-        print("Vocabulary of Word:", self.vocab_word)
-
-        indexed = []
-        for word in tokens:
-            temp = []
-            for char in list(word):
-                temp.append(self.char2idx[char])
-            if len(temp) < self.max_word_len:
-                temp += [0] * (self.max_word_len - len(temp))
-            indexed.append(temp)
-        self.char_indexed = np.array(indexed) # (None, self.max_word_len)
-        print("Char indexed: ", self.char_indexed.shape)
-
-        self.word_indexed = np.array([self.word2idx[word] for word in tokens])
-        print("Word indexed: ", self.word_indexed.shape)
-    # end method text_preprocessing
+        self.train_op = tf.train.AdamOptimizer().apply_gradients(zip(clipped_gradients, params))
+    # end method
 
 
     def next_batch(self, batch_size, text_iter_step):
@@ -196,13 +129,12 @@ class ConvRNNTextGen:
         for i in range(0, len(self.word_indexed)-window-1, text_iter_step):
             yield (self.char_indexed[i : i+window].reshape(-1, self.seq_len, self.max_word_len),
                    self.word_indexed[i+1 : i+window+1].reshape(-1, self.seq_len))
-    # end method next_batch
+    # end method
 
 
     def fit(self, start_word, text_iter_step=25, n_gen=50, n_epoch=1, batch_size=128, en_exp_decay=False):
         global_step = 0
         n_batch = (len(self.word_indexed) - self.seq_len*batch_size - 1) // text_iter_step
-        total_steps = n_epoch * n_batch
         self.sess.run(tf.global_variables_initializer()) # initialize all variables
         """
         if os.path.isfile(self.model_path+'.meta'):
@@ -212,13 +144,10 @@ class ConvRNNTextGen:
         for epoch in range(n_epoch):
             next_state = self.sess.run(self.init_state, {self.batch_size: batch_size})
             for local_step, (X_batch, Y_batch) in enumerate(self.next_batch(batch_size, text_iter_step)):
-                lr = self.adjust_lr(global_step, total_steps) if en_exp_decay else 0.001
                 _, train_loss, next_state = self.sess.run([self.train_op, self.loss, self.final_state],
                                                           {self.X: X_batch,
                                                            self.Y: Y_batch,
-                                                           self.init_state: next_state,
-                                                           self.lr: lr})
-                
+                                                           self.init_state: next_state})
                 print ('Epoch %d/%d | Batch %d/%d | train loss: %.4f'
                         % (epoch+1, n_epoch, local_step, n_batch, train_loss))
                 if local_step % 10 == 0:
@@ -230,7 +159,7 @@ class ConvRNNTextGen:
                 global_step += 1
             
         return log
-    # end method fit
+    # end method
 
 
     def infer(self, start_word, n_gen):
@@ -251,5 +180,43 @@ class ConvRNNTextGen:
             chars = list(word) 
             char_indices = [self.char2idx[c] for c in chars] + [0] * (self.max_word_len - len(chars))
         return out_sentence
-    # end method infer
+    # end method
+
+
+    def preprocessing(self):
+        text = self.text
+        text = text.replace('\n', ' \n ')
+        text = text.replace('  ', ' ')
+        text = text.lower()
+        tokens = text.split(' ')
+        # print(tokens)
+
+        chars = set(text)
+        self.char2idx = {c: i+1 for i, c in enumerate(chars)}
+        self.char2idx['<PAD>'] = 0
+        self.idx2char = {i: c for c, i in self.char2idx.items()}
+        self.vocab_char = len(self.idx2char)
+        print("Vocabulary of Char:", self.vocab_char)
+
+        words = set(tokens)
+        self.max_word_len = max([len(w) for w in words])
+        self.word2idx = {w: i for i, w in enumerate(words)}
+        self.idx2word = {i: w for i, w in enumerate(words)}
+        self.vocab_word = len(self.word2idx)
+        print("Vocabulary of Word:", self.vocab_word)
+
+        indexed = []
+        for word in tokens:
+            temp = []
+            for char in list(word):
+                temp.append(self.char2idx[char])
+            if len(temp) < self.max_word_len:
+                temp += [0] * (self.max_word_len - len(temp))
+            indexed.append(temp)
+        self.char_indexed = np.array(indexed)
+        print("Char indexed: ", self.char_indexed.shape)
+
+        self.word_indexed = np.array([self.word2idx[word] for word in tokens])
+        print("Word indexed: ", self.word_indexed.shape)
+    # end method
 # end class

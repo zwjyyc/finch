06ee1e2214640336e878083dc7fb513d8cd310b8
Zhedong Zheng <zhedongzheng@Zhedongs-MacBook-Pro.local>
2017-11-02 14:00:29 +0000

    fix test case

--- nlp-models/tensorflow/attn_is_all_u_need/train.py
+++ nlp-models/tensorflow/attn_is_all_u_need/train.py
@@ -23,25 +23,28 @@ def main():
         x={'source':sources, 'target':targets}, batch_size=args.batch_size, num_epochs=args.num_epochs,
         shuffle=True))
     
-    stupid_decode_test('apple', tf_estimator, dl)
-    stupid_decode_test('common', tf_estimator, dl)
+    stupid_decode_test(['apple', 'common', 'zhedong'], tf_estimator, dl, test_maxlen=7)
 
 
-def stupid_decode_test(test_word, tf_estimator, dl, test_maxlen=7):
-    test_idx = [dl.source_word2idx[c] for c in test_word] + \
-            [dl.source_word2idx['<pad>']] * (test_maxlen - len(test_word))
-    test_idx = np.atleast_2d(test_idx)
+def stupid_decode_test(test_words, tf_estimator, dl, test_maxlen):
+    test_indices = []
+    for test_word in test_words:
+        test_idx = [dl.source_word2idx[c] for c in test_word] + \
+                [dl.source_word2idx['<pad>']] * (test_maxlen - len(test_word))
+        test_indices.append(test_idx)
+    test_indices = np.atleast_2d(test_indices)
     
-    pred_ids = np.zeros([1, test_maxlen], np.int64)
+    pred_ids = np.zeros([len(test_words), test_maxlen], np.int64)
     for j in range(test_maxlen):
         _pred_ids = tf_estimator.predict(tf.estimator.inputs.numpy_input_fn(
-            x={'source':test_idx, 'target':pred_ids}, batch_size=1, shuffle=False))
+            x={'source':test_indices, 'target':pred_ids}, batch_size=len(test_words), shuffle=False))
         _pred_ids = np.array(list(_pred_ids))
         pred_ids[:, j] = _pred_ids[:, j]
     
     target_idx2word = {i: w for w, i in dl.target_word2idx.items()}
-    ans = ''.join([target_idx2word[id] for id in pred_ids[0]])
-    print(test_word, '->', ans)
+    for i, test_word in enumerate(test_words):
+        ans = ''.join([target_idx2word[id] for id in pred_ids[i]])
+        print(test_word, '->', ans)
 
 
 if __name__ == '__main__':

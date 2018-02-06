d3c72622117db4bc1858a32b955588562f3911bc
Yichun Yin <ssggjjdd@163.com>
2018-02-06 17:32:13 +0800

    minor changes

--- nlp-models/tensorflow/seq2seq_attn.py
+++ nlp-models/tensorflow/seq2seq_attn.py
@@ -48,8 +48,8 @@ class Seq2Seq:
         self.batch_size = tf.placeholder(tf.int32, [])
 
     def restore_graph(self):
-        self.saver = tf.train.import_meta_graph('./my_test_model.meta')
-        self.saver.restore(self.sess, tf.train.latest_checkpoint('./'))
+        # self.saver = tf.train.import_meta_graph('./my_test_model.meta')
+        self.saver.restore(self.sess, './model.ckpt')
 
     def build_graph(self):
         self.build_encoder_layer()
@@ -182,7 +182,7 @@ class Seq2Seq:
                   % (epoch, n_epoch, val_loss))
             if val_loss < best_metric_val:
                 print 'Storing the model'
-                self.saver.save(self.sess, './my_test_model')  # self.model_dir)
+                self.saver.save(self.sess, './model.ckpt')  # self.model_dir)
                 best_metric_val = val_loss
 
     def infer_sentence(self, input_word, x_idx2word, y_idx2word, batch_size=128):
